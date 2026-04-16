4fab9dd41aed908f53f1f81c7301db2cc8981583
Eduardo Almadana Clérisse <eduardo@tsf-460-wpa-1-110.epfl.ch>
2026-04-16 23:02:46 +0200

    Prototype of radar plot made

--- appDev/main.py
+++ appDev/main.py
@@ -1,25 +1,19 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+
+import radar_plot
 
 def main():
-    categories = ['Speed', 'Power', 'Skill', 'Agility', 'Stamina']
-    values = [3, 4, 5, 2, 4]
+    st.write("""# Open Source Meditation App""")
 
-    N = len(categories)
-    angles = np.linspace(0, 2*np.pi, N, endpoint=False).tolist()
 
-    values += values[:1]
-    angles += angles[:1]
 
-    fig, ax = plt.subplots(subplot_kw=dict(polar=True))
+# Plotting the radar plot
+    radar_plot.create_radar_plot()
 
-    ax.plot(angles, values)
-    ax.fill(angles, values, alpha=0.25)
 
-    ax.set_xticks(angles[:-1])
-    ax.set_xticklabels(categories)
 
-    st.pyplot(fig)
+
+
+
 
 if __name__ == "__main__":    main()
